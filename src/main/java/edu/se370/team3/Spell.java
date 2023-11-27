f1184f7bfb247fdc9ecadfc4cd1086edd11ccb63
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2023-11-27 13:34:58 -0800

    Got SpellLibrary to load in all spells
    Spell's are sorted into two ArrayLists, one for Level 1 spells, and the other for Cantrips

--- src/main/java/edu/se370/team3/Spell.java
+++ src/main/java/edu/se370/team3/Spell.java
@@ -1,236 +1,245 @@
 package edu.se370.team3;
 
 public class Spell {
-    public enum DamageType {
-        PSYCHIC,
-        LIGHTNING,
-        RADIANT,
-        ACID,
-        BLUDGEONING,
-        COLD,
-        FIRE,
-        FORCE,
-        NECROTIC,
-        POISON,
-        THUNDER
-    };
+  public enum DamageType {
+    PSYCHIC,
+    LIGHTNING,
+    RADIANT,
+    ACID,
+    BLUDGEONING,
+    COLD,
+    FIRE,
+    FORCE,
+    NECROTIC,
+    POISON,
+    THUNDER,
+    NONE
+  };
 
-    public enum School {
-        DIVINATION,
-        NECROMANCY,
-        EVOCATION,
-        ILLUSION,
-        ABJURATION,
-        CONJURATION,
-        TRANSMUTATION,
-        ENCHANTMENT
-    };
+  public enum School {
+    DIVINATION,
+    NECROMANCY,
+    EVOCATION,
+    ILLUSION,
+    ABJURATION,
+    CONJURATION,
+    TRANSMUTATION,
+    ENCHANTMENT,
+    NONE
+  };
 
-    private String name;
-    private String description;
-    private int level; // 0: Cantrip, 1: Level 1
-    private int range;
-    private DamageType damageType;
-    private School school;
-    private int duration;
-    private Time time;
+  private String name;
+  private String description;
+  private int level; // 0: Cantrip, 1: Level 1
+  private int range;
+  private DamageType damageType;
+  private School school;
+  private String durationType;
+  private int duration;
+  private Time time;
 
-    public Spell(
-            String name,
-            int level,
-            String school,
-            int rounds,
-            String actionType,
-            int range,
-            String damageType,
-            int duration,
-            String description) {
-        this.name = name;
-        this.description = description;
-        this.level = level;
-        this.range = range;
-        this.duration = duration;
-        this.time = new Time(rounds, actionType);
+  public Spell(
+      String name,
+      int level,
+      String school,
+      int rounds,
+      String actionType,
+      int range,
+      String damageType,
+      String durationType,
+      int duration,
+      String description) {
+    this.name = name;
+    this.description = description;
+    this.level = level;
+    this.range = range;
+    this.durationType = durationType;
+    this.duration = duration;
+    this.time = new Time(rounds, actionType);
 
-        damageType = damageType.toLowerCase();
+    damageType = damageType.toLowerCase();
 
-        switch (damageType) {
-            case "psychic":
-                this.damageType = DamageType.PSYCHIC;
-                break;
-            case "lightning":
-                this.damageType = DamageType.LIGHTNING;
-                break;
-            case "radiant":
-                this.damageType = DamageType.RADIANT;
-                break;
-            case "acid":
-                this.damageType = DamageType.ACID;
-                break;
-            case "bludgeoning":
-                this.damageType = DamageType.BLUDGEONING;
-                break;
-            case "cold":
-                this.damageType = DamageType.COLD;
-                break;
-            case "fire":
-                this.damageType = DamageType.FIRE;
-                break;
-            case "force":
-                this.damageType = DamageType.FORCE;
-                break;
-            case "necrotic":
-                this.damageType = DamageType.NECROTIC;
-                break;
-            case "poison":
-                this.damageType = DamageType.POISON;
-                break;
-            case "thunder":
-                this.damageType = DamageType.THUNDER;
-                break;
-            default:
-                this.damageType = DamageType.FORCE;
-                break;
-        }
-
-        switch (school) {
-            case "D":
-            case "d":
-                this.school = School.DIVINATION;
-                break;
-            case "N":
-            case "n":
-                this.school = School.NECROMANCY;
-                break;
-            case "V":
-            case "v":
-                this.school = School.EVOCATION;
-                break;
-            case "I":
-            case "i":
-                this.school = School.ILLUSION;
-                break;
-            case "A":
-            case "a":
-                this.school = School.ABJURATION;
-                break;
-            case "C":
-            case "c":
-                this.school = School.CONJURATION;
-                break;
-            case "T":
-            case "t":
-                this.school = School.TRANSMUTATION;
-                break;
-            case "E":
-            case "e":
-                this.school = School.ENCHANTMENT;
-                break;
-            default:
-                this.school = School.DIVINATION;
-                break;
-        }
-
+    switch (damageType) {
+      case "psychic":
+        this.damageType = DamageType.PSYCHIC;
+        break;
+      case "lightning":
+        this.damageType = DamageType.LIGHTNING;
+        break;
+      case "radiant":
+        this.damageType = DamageType.RADIANT;
+        break;
+      case "acid":
+        this.damageType = DamageType.ACID;
+        break;
+      case "bludgeoning":
+        this.damageType = DamageType.BLUDGEONING;
+        break;
+      case "cold":
+        this.damageType = DamageType.COLD;
+        break;
+      case "fire":
+        this.damageType = DamageType.FIRE;
+        break;
+      case "force":
+        this.damageType = DamageType.FORCE;
+        break;
+      case "necrotic":
+        this.damageType = DamageType.NECROTIC;
+        break;
+      case "poison":
+        this.damageType = DamageType.POISON;
+        break;
+      case "thunder":
+        this.damageType = DamageType.THUNDER;
+        break;
+      default:
+        this.damageType = DamageType.NONE;
+        break;
     }
 
-    public Spell() {
-        this("Eldritch Blast",
-                0,
-                "V",
-                1,
-                "action",
-                120,
-                "force",
-                0,
-                "Eldritch blast paaaawnch");
+    switch (school) {
+      case "D":
+      case "d":
+        this.school = School.DIVINATION;
+        break;
+      case "N":
+      case "n":
+        this.school = School.NECROMANCY;
+        break;
+      case "V":
+      case "v":
+        this.school = School.EVOCATION;
+        break;
+      case "I":
+      case "i":
+        this.school = School.ILLUSION;
+        break;
+      case "A":
+      case "a":
+        this.school = School.ABJURATION;
+        break;
+      case "C":
+      case "c":
+        this.school = School.CONJURATION;
+        break;
+      case "T":
+      case "t":
+        this.school = School.TRANSMUTATION;
+        break;
+      case "E":
+      case "e":
+        this.school = School.ENCHANTMENT;
+        break;
+      default:
+        this.school = School.NONE;
+        break;
     }
 
-    public String getLevel() {
-        switch (this.level) {
-            case 0:
-                return "Cantrip";
-            case 1:
-                return "Level 1 Spell";
-            default:
-                return "Level -1 Spell";
-        }
+  }
+
+  public Spell() {
+    this("Eldritch Blast",
+        0,
+        "V",
+        1,
+        "action",
+        120,
+        "force",
+        "instant",
+        0,
+        "Eldritch blast paaaawnch");
+  }
+
+  public String getLevel() {
+    switch (this.level) {
+      case 0:
+        return "Cantrip";
+      case 1:
+        return "Level 1 Spell";
+      default:
+        return "Level -1 Spell";
     }
+  }
 
-    @Override
-    public String toString() {
-        String output = "";
+  @Override
+  public String toString() {
+    String output = "";
 
-        output += "========== " + this.name + " ==========\n";
-        output += "Spell Level: " + this.getLevel();
-        output += "\nRange: " + this.range + " meters";
-        output += "\nDamage Type: " + this.damageType;
-        output += "\nSchool of Magic: " + this.school;
-        output += "\nDuration: " + this.duration + " rounds";
-        output += "\nCast time: " + this.time;
-        output += "\n======================";
-        for (int i = 0; i < this.name.length(); i++) {
-            output += "=";
-        }
-        output += "\nDescription: " + this.description;
-        return output;
+    output += "========== " + this.name + " ==========\n";
+    output += "Spell Level: " + this.getLevel();
+    output += "\nRange: " + this.range + " meters";
+    output += "\nDamage Type: " + this.damageType;
+    output += "\nSchool of Magic: " + this.school;
+    if (duration != 0) {
+      output += "\nDuration: This spell lasts for " + duration + " " + durationType;
+      if (duration > 1)
+        output += "s";
+      output += ".";
     }
+    output += "\nCast time: " + this.time;
+    output += "\n";
+    output += "\nDescription: " + this.description;
+    output += "\n";
+    return output;
+  }
 
-    public static void main(String[] args) {
-        Spell testSpell = new Spell();
+  public static void main(String[] args) {
+    Spell testSpell = new Spell();
 
-        System.out.println(testSpell);
-    }
+    System.out.println(testSpell);
+  }
 }
 
 class Time {
-    public enum Unit {
-        ACTION,
-        BONUS
+  public enum Unit {
+    ACTION,
+    BONUS
+  }
+
+  private int number;
+  private Unit unit;
+
+  public Time(int number, Unit unit) {
+    this.number = number;
+    this.unit = unit;
+  }
+
+  public Time(int number, String unit) {
+    this.number = number;
+    unit = unit.toLowerCase();
+
+    switch (unit) {
+      case "action":
+        this.unit = Unit.ACTION;
+        break;
+      case "bonus":
+        this.unit = Unit.BONUS;
+        break;
+      default:
+        this.unit = Unit.ACTION;
+        break;
     }
+  }
 
-    private int number;
-    private Unit unit;
+  public int getRounds() {
+    return this.number;
+  }
 
-    public Time(int number, Unit unit) {
-        this.number = number;
-        this.unit = unit;
-    }
+  public void setRounds(int num) {
+    this.number = num;
+  }
 
-    public Time(int number, String unit) {
-        this.number = number;
-        unit = unit.toLowerCase();
+  public Unit getTypeOfAction() {
+    return unit;
+  }
 
-        switch (unit) {
-            case "action":
-                this.unit = Unit.ACTION;
-                break;
-            case "bonus":
-                this.unit = Unit.BONUS;
-                break;
-            default:
-                this.unit = Unit.ACTION;
-                break;
-        }
-    }
+  public void setTypeOfAction(Unit unit) {
+    this.unit = unit;
+  }
 
-    public int getRounds() {
-        return this.number;
-    }
-
-    public void setRounds(int num) {
-        this.number = num;
-    }
-
-    public Unit getTypeOfAction() {
-        return unit;
-    }
-
-    public void setTypeOfAction(Unit unit) {
-        this.unit = unit;
-    }
-
-    @Override
-    public String toString() {
-        return this.number + " round, using up your " + this.unit + " slot";
-    }
+  @Override
+  public String toString() {
+    return this.number + " round, using up your " + this.unit + " slot";
+  }
 }
--- src/main/java/edu/se370/team3/SpellLibrary.java
+++ src/main/java/edu/se370/team3/SpellLibrary.java
@@ -10,64 +10,66 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SpellLibrary {
   private ArrayList<Spell> level1SpellList;
   private ArrayList<Spell> cantripList;
-  private static ObjectMapper objMapper = getDefaultObjMapper();
 
   public SpellLibrary() {
+    String spellFile = "lib/1stspellsCantrips.json";
+    ObjectMapper objectMapper = new ObjectMapper();
+    this.level1SpellList = new ArrayList<Spell>();
+    this.cantripList = new ArrayList<Spell>();
 
-  }
+    try {
 
-  private static ObjectMapper getDefaultObjMapper() {
-    ObjectMapper defaultObjMapper = new ObjectMapper();
+      JsonNode jsonNode = objectMapper.readTree(new File(spellFile));
 
-    return defaultObjMapper;
-  }
+      for (JsonNode entry : jsonNode) {
 
-  public static JsonNode parse(String src) throws IOException {
-    return objMapper.readTree(src);
+        String name = entry.get("name").asText();
+        int level = entry.get("level").asInt();
+        String school = entry.get("school").asText();
+        int rounds = entry.get("time").get(0).get("number").asInt();
+        String actionType = entry.get("time").get(0).get("unit").asText();
+        int range = 0;
+        if (entry.get("range").get("distance").has("amount")) {
+          range = entry.get("range").get("distance").get("amount").asInt();
+        }
+        String damageType = "none";
+        if (entry.has("damageInflict")) { // Chromatic Orb needs to be a special case
+          damageType = entry.get("damageInflict").get(0).asText();
+        }
+        String durationType = "instant";
+        int duration = 0;
+        if (entry.get("duration").get(0).get("type").asText().equals("timed")) {
+          durationType = entry.get("duration").get(0).get("duration").get("type").asText();
+          duration = entry.get("duration").get(0).get("duration").get("amount").asInt();
+        }
+        String description = "";
+        for (JsonNode desc : entry.get("entries")) {
+          description += desc.asText() + " ";
+        }
+
+        Spell spell = new Spell(name, level, school, rounds, actionType, range, damageType, durationType, duration,
+            description);
+
+        if (level == 0) {
+          this.cantripList.add(spell);
+        } else {
+          this.level1SpellList.add(spell);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
-    String spellFile = "lib/1stspellsCantrips.json";
-    ObjectMapper objectMapper = new ObjectMapper();
-    ArrayList<Spell> level1SpellList = new ArrayList<Spell>();
-    ArrayList<Spell> cantripList = new ArrayList<Spell>();
 
-    try {
-      JsonNode jsonNode = objectMapper.readTree(new File(spellFile));
+    SpellLibrary test = new SpellLibrary();
 
-      String name = jsonNode.get(0).get("name").asText();
-      System.out.println("Name: " + name);
+    System.out.println("Number of cantrips: " + test.cantripList.size());
+    System.out.println("Number of level 1 spells: " + test.level1SpellList.size());
+    System.out.println();
+    System.out.println(test.cantripList.get(13));
+    System.out.println(test.level1SpellList.get(0));
 
-      int level = jsonNode.get(0).get("level").asInt();
-      System.out.println("Level: " + level);
-
-      String school = jsonNode.get(0).get("school").asText();
-      System.out.println("School: " + school);
-
-      int rounds = jsonNode.get(0).get("time").get(0).get("number").asInt();
-      System.out.println("Rounds: " + rounds);
-
-      String actionType = jsonNode.get(0).get("time").get(0).get("unit").asText();
-      System.out.println("Action type: " + actionType);
-
-      int range = jsonNode.get(0).get("range").get("distance").get("amount").asInt();
-      System.out.println("Range: " + range + " ft");
-
-      // Chromatic Orb needs to be a special case
-      String damageType = jsonNode.get(0).get("damageInflict").get(0).asText();
-      System.out.println("Damage Type: " + damageType);
-
-      // if
-      // (jsonNode.get(0).get("duration").get(0).get("type").toString().equals("instant"))
-      // {
-      String duration = jsonNode.get(0).get("duration").get(0).get("type").asText();
-      System.out.println("Duration: " + duration);
-      // }
-
-    } catch (
-
-    IOException e) {
-      e.printStackTrace();
-    }
   }
 }
